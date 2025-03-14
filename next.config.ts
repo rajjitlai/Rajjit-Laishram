469005e1c69889ff28acc72f268380456ed3fb42
RAJJIT <2.0lairaj@gmail.com>
2025-03-14 21:00:57 +0530

    fixed images config

--- next.config.ts
+++ next.config.ts
@@ -2,19 +2,18 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   images: {
-    domains: ["cloud.appwrite.io"],
     remotePatterns: [
       {
         protocol: "https",
         hostname: "cloud.appwrite.io",
-        pathname: "/v1/storage/buckets/**", 
+        pathname: "/v1/storage/buckets/**",
       },
     ],
   },
   async redirects() {
     return [
       {
-        source: "/v1/storage/:slug*", 
+        source: "/v1/storage/:slug*",
         destination: "/buckets/:slug*",
         permanent: true,
       },
