7a9c37df3aa000190ed401f73d27302830c91e02
RAJJIT <2.0lairaj@gmail.com>
2025-04-07 12:54:06 +0530

    fixed tag

--- app/layout.tsx
+++ app/layout.tsx
@@ -56,7 +56,7 @@ export default function RootLayout({
               window.dataLayer = window.dataLayer || [];
               function gtag(){dataLayer.push(arguments);}
               gtag('js', new Date());
-              gtag('config', 'G-HDYCTV3EVL', {
+              gtag('config', 'G-XXXXXXXXXX', {
                 page_path: window.location.pathname,
               });
             `,
