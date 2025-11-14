9287c95d256e5443dbc72d0f0c5d25b4d16c7c8b
RAJJIT <2.0lairaj@gmail.com>
2025-11-14 15:13:56 +0530

    feat: removed testimonials and bg changes

--- app/(home)/page.tsx
+++ app/(home)/page.tsx
@@ -9,25 +9,25 @@ import Projects from "./components/Projects";
 import Footer from "./components/Footer";
 import { Contact } from "./components/Contact";
 import { Experience } from "./components/Experience";
-import { Testimonials } from "./components/Testimonials";
+// import { Testimonials } from "./components/Testimonials";
 import { FloatNav } from "./components/FloatNav";
 
 export default function page() {
   return (
-    <div className="min-h-screen bg-black overflow-hidden">
+    <div className="min-h-screen bg-black overflow-hidden relative">
+      <BackgroundBeams />
       <FloatNav />
-      <div className="max-w-7xl mx-auto p-5">
-        <BackgroundBeams />
+      <div className="max-w-7xl mx-auto p-5 relative z-10">
         <Navbar />
         <Hero />
       </div>
-      <div className="max-w-7xl mx-auto p-5 mt-20">
+      <div className="max-w-7xl mx-auto p-5 mt-20 relative z-10">
         <Skills />
         <Projects />
         <Experience />
       </div>
-      <div className="max-w-7xl mx-auto p-5 mt-10">
-        <Testimonials />
+      <div className="max-w-7xl mx-auto p-5 mt-10 relative z-10">
+        {/* <Testimonials /> */}
         <Contact />
       </div>
       <Footer />
